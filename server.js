cb469817c3a09977c119c9b9ca65a35a521526a6
Tanguyix <tboissel@student.42.fr>
2019-04-05 14:16:14 +0200

    some basic verif of user_creation done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const nodeadmin = require('nodeadmin');
-const crypto = require('crypto');
+const crypto = require('crypto'); //a remplacer par bcrypt
 const pw_hash = require('password-hash');
 
 let connection = mysql.createConnection({
@@ -21,7 +21,7 @@ connection.connect(function(err) {
 
 app.use(nodeadmin(app));
 
-app.post('/api/login', (req, res) => {
+app.post('/api/create', (req, res) => {
     let response = {
         email : req.query.email,
         login : req.query.login,
@@ -32,15 +32,38 @@ app.post('/api/login', (req, res) => {
         re_pw : req.query.confirm
     }
 
+    //Check if every field is full
+    if (typeof response.email == 'undefined' || response.email == '' ||  typeof response.login == 'undefined' || response.login == '' || typeof response.firstName == 'undefined'|| response.firstName == '' || typeof response.lastName == 'undefined' || response.lastName == '' || typeof response.gender == 'undefined' || response.gender == '' || typeof response.password == 'undefined' || response.password == '' || typeof response.re_pw == 'undefined' || response.re_pw == '') {
+        res.status(500);
+        res.end("Empty field");
+    }
+    //Check if passwords are both equal
+    else if (response.password != response.re_pw) {
+        res.status(500);
+        res.end("Passwords don't match");
+    }
+
+    //Check is password is long and good enough (see with Flo)
+
+    //Check if login is unique (see how to do multiple queries)
+
+    //Check if gender is either Male or female
+
+    //Check if firtsname is correct
+
+    //Check if lastname is correct
+
     let hashed_pw = pw_hash.generate(response.password);
 
-    const sql = "INSERT INTO users(login, firstName, lastName, gender, email, pwd) " +
-        `VALUES("${response.login}", "${response.firstName}", "${response.lastName}", "${response.gender}", "${response.email}", "${hashed_pw}");`;
 
-     connection.query(sql , (err, result) => {
-         if (err) throw err;
-         console.log("Result: " + result);
-     })
+    // const sql = "INSERT INTO users(login, firstName, lastName, gender, email, pwd) " +
+    //     `VALUES("${response.login}", "${response.firstName}", "${response.lastName}", "${response.gender}", "${response.email}", "${hashed_pw}");`;
+    //
+    //  connection.query(sql , (err, result) => {
+    //      if (err) throw err;
+    //      console.log("Result: " + result);
+    //  })
+    res.end("User created");
 });
 
 
