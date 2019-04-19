70eabed0faf48bd60964b1ee3c6f1b1808e333b1
Florent Klein <flklein@student.42.fr>
2019-04-19 14:00:54 +0200

    loading screen + background fix for profile pic

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 
-// import '../css/loading.css';
+import '../css/loading.css';
 import loadingImg from '../assets/img/loading.svg';
 
 class Loading extends Component {
   render() {
     return (
       <div className="centered">
-        {/*<img className="loading" src={loadingImg} alt="loading"/>*/}
+        <img className="loading" src={loadingImg} alt="loading"/>
       </div>
     );
   }
