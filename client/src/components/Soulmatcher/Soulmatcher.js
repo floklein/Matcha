8ead75079c46283bccafeeff346c2eac9f0766e2
Florent Klein <flklein@student.42.fr>
2019-04-22 13:22:51 +0200

    slider

--- client/src/components/Soulmatcher/Soulmatcher.js
+++ client/src/components/Soulmatcher/Soulmatcher.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import classnames from "classnames";
+import noUiSlider from 'nouislider';
 
 import Card from "./Card";
 
 import './soulmatcher.css';
 
 class Soulmatcher extends Component {
+  componentDidMount() {
+    let sliderAge = document.getElementById('age');
+
+    noUiSlider.create(sliderAge, {
+      start: [18, 100],
+      connect: true,
+      range: {
+        'min': 18,
+        'max': 100
+      }
+    });
+  }
+
   onLike = () => {
     const card = document.querySelector('div.card:last-child');
     if (!card) return;
@@ -34,7 +49,30 @@ class Soulmatcher extends Component {
         <div className="centered this">
           <div id="soulmatcher">
             <div className="sidebar">
-
+              <div className="sidebar__sort">
+                <div className="sidebar__title">Trier par</div>
+                <select name="sort" title="sort" required onChange={() => {}} defaultValue="pertinence">
+                  <option value="" disabled>Trier par...</option>
+                  <option value="relevance">Pertinence</option>
+                  <option value="age">Âge</option>
+                  <option value="distance">Distance</option>
+                  <option value="popularity">Popularité</option>
+                  <option value="interests">Intérêts</option>
+                </select>
+                <select name="order" title="order" required onChange={() => {}} defaultValue="asc">
+                  <option value="" disabled>En ordre...</option>
+                  <option value="asc">Croissant</option>
+                  <option value="desc">Décroissant</option>
+                </select>
+              </div>
+              <div className="sidebar__filter">
+                <div className="sidebar__title">Filtrer par</div>
+                <div id="age"/>
+                {/*<input type="text"/>*/}
+                {/*<input type="text"/>*/}
+                {/*<input type="text"/>*/}
+                {/*<input type="text"/>*/}
+              </div>
             </div>
             <div className="main-panel">
               <button className="dislike purple" onClick={this.onDislike}/>
