a5cd05089e5f84ea0d64575904bb6edc0358910b
Tanguyix <tboissel@student.42.fr>
2019-04-27 16:57:46 +0200

    debug picture deleting and add make profile picture

--- routes/api/picture.js
+++ routes/api/picture.js
@@ -22,9 +22,8 @@ router.delete('/:pic_nb', (req, res) =>{
   if (user.id === -1) {
     return res.status(401).json({error: 'unauthorized access'});
   }
-console.log(user.id);
 
-  if (!req.params.pic_nb || req.params.pic_nb > 5) {
+  if (!req.params.pic_nb || req.params.pic_nb > 5 || isNaN(req.params.pic_nb)) {
     return res.status(400).json({pic_nb: "Le numero de la photo est incorrect"});
   }
   let sql = `SELECT pic${req.params.pic_nb} as pic FROM photos ` +
@@ -35,14 +34,37 @@ console.log(user.id);
     connection.query(sql, (err) => {
       if (err) throw err;
       let pic_to_del = pic[0].pic;
-      console.log(pic_to_del);
       sql = `UPDATE infos set profile_pic = NULL WHERE user_id = ${user.id} and profile_pic = "${pic_to_del}";`;
-      console.log(sql);
       connection.query(sql, (err) => {
         if (err) throw err;
         return res.json();
       })
     })
+  })
+});
+
+router.post('/profile_pic/:pic_nb', (req, res) => {
+  const user = jwt_check.getUsersInfos(req.headers.authorization);
+  if (user.id === -1) {
+    return res.status(401).json({error: 'unauthorized access'});
+  }
+  if (!req.params.pic_nb || req.params.pic_nb > 5 || isNaN(req.params.pic_nb)) {
+    return res.status(400).json({pic_nb: "Le numero de la photo est incorrect"});
+  }
+  let sql = `SELECT pic${req.params.pic_nb} as pic FROM photos ` +
+    `WHERE user_id = ${user.id};`;
+  connection.query(sql, (err, pic) => {
+    if (err) throw err;
+    if (pic[0].pic !== null) {
+      sql = `UPDATE infos SET profile_pic = "${pic[0].pic}" WHERE user_id = ${user.id};`;
+      connection.query(sql, (err) => {
+        if (err) throw err;
+        return res.json();
+      })
+    }
+    else {
+      return res.status(400).json({pic: "La photo sélectionnée n'existe pas"})
+    }
   })
 });
 
