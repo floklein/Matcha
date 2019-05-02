c535d40b725727cb0421f135cb7149d8c0a9574d
Tanguyix <tboissel@student.42.fr>
2019-05-02 13:21:06 +0200

    post message done

--- routes/api/chat.js
+++ routes/api/chat.js
@@ -24,7 +24,6 @@ router.get('/', (req, res) => {
     return res.status(401).json({error: 'unauthorized access'});
   }
   let match_id = req.body.id;
-  let response = [];
 
   const sql = "SELECT sender_id, receiver_id, message, time FROM messages " +
     `WHERE (sender_id = ${match_id} AND receiver_id = ${user.id}) OR (sender_id = ${user.id} AND receiver_id = ${match_id}) ` +
@@ -33,6 +32,37 @@ router.get('/', (req, res) => {
     if (err) throw err;
     return res.json(resp);
   })
+});
+
+router.post('/', (req, res) => {
+  const user = jwt_check.getUsersInfos(req.headers.authorization);
+  if (user.id === -1) {
+    return res.status(401).json({error: 'unauthorized access'});
+  }
+  let match_id = req.body.id;
+  let message = req.body.message;
+
+  let sql = "SELECT id FROM likes " +
+    `WHERE (liker_id = ${match_id} AND liked_id = ${user.id}) OR (liked_id = ${match_id} AND liker_id = ${user.id});`;
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    if (result.length !== 2)
+      return res.status(400).json({id: "Vous ne pouvez envoyer de message qu'aux utilisateurs déjà matchés"});
+    sql = "SELECT id from blocks " +
+      `WHERE (blocker_id = ${match_id} AND blocked_id = ${user.id}) OR (blocked_id = ${match_id} AND blocker_id = ${user.id});`;
+    connection.query(sql, (err, result) => {
+      if (err) throw err;
+      if (result.length)
+        return res.status(400).json({id: "Vous ne pouvez envoyer de message aux utilisateurs bloqués / qui vous ont bloqués"});
+
+      sql = "INSERT INTO messages(sender_id, receiver_id, message, time) " +
+        `VALUES(${user.id}, ${match_id}, "${message}", now());`;
+      connection.query(sql, (err, resp) => {
+        if (err) throw err;
+        return res.json({});
+      })
+    })
+  });
 });
 
 module.exports = router;
