aac3e3e5ce525cd2ea8040b2f82e91974318869f
Tanguyix <tboissel@student.42.fr>
2019-05-11 13:00:09 +0200

    notif settings back-end done

--- utils/notifs.js
+++ utils/notifs.js
@@ -15,20 +15,24 @@ connection.connect(function (err) {
 
 module.exports = {
   postNotif: function postNotif(id, type, content, user_id, notifier_name) {
-    //TODO: verif data
-
     let sql = "SELECT id from blocks " +
       `WHERE (blocker_id = ${user_id} AND blocked_id = ${id}) OR (blocked_id = ${user_id} AND blocker_id = ${id})`;
     connection.query(sql, (err, res) => {
       if (err) throw err;
       if (res.length)
         return;
-      io.sockets.to(`r${id}`).emit('new notif', {content, type});
-      sql = "INSERT INTO notifs(user_id, type, content, notifier_name, time) " +
-        `VALUES(${id}, "${type}", "${content}", "${notifier_name}", now());`;
-      connection.query(sql, (err) => {
+      sql = `SELECT ` + "`" + type  + "`" +  `as type from settings WHERE user_id = ${id};`;
+      connection.query(sql, (err, res) => {
         if (err) throw err;
-      })
+        if (res[0].type) {
+          io.sockets.to(`r${id}`).emit('new notif', {content, type});
+          sql = "INSERT INTO notifs(user_id, type, content, notifier_name, time) " +
+            `VALUES(${id}, "${type}", "${content}", "${notifier_name}", now());`;
+          connection.query(sql, (err) => {
+            if (err) throw err;
+          })
+        }
+      });
     });
   }
 };
