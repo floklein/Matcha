8b1b2f5c24ad115780ed6ac095ba2a565c62807e
flklein <flo-klein@hotmail.fr>
2019-05-31 02:43:56 +0200

    static folder

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -29,12 +28,7 @@ app.use('/api/notifs', require('./routes/api/notifs'));
 app.use('/api/connection', require('./routes/api/connection'));
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.use(express.static('client/build'));
 
 
 const port = process.env.PORT || 5000;
